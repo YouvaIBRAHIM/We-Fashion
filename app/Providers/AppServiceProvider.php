d02a57ae4eb615e33a6e706d13beccfcac131d21
YouvaIBRAHIM <youva.ibrahim@gmail.com>
2023-04-30 22:45:34 +0200

    updated README file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        $categories = Category::all();
-        View::share('categories', $categories);
+        // $categories = Category::all();
+        // View::share('categories', $categories);
     }
 }
